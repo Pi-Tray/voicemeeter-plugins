17909bb80d2bbfb8e22c4465f3f40f53b0e1fac6
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-08-05 20:20:38 +0100

    install and use types

--- src/play_sound/index.ts
+++ src/play_sound/index.ts
@@ -1,45 +1,43 @@
 import path from "path";
 import fs from "fs";
 
+import type {Plugin} from "pi-tray-server/src/types";
+
 import voicemeeter from "voicemeeter-remote";
 
-export const display_name = "Play sound with Voicemeeter";
+export default {
+    display_name: "Play sound with Voicemeeter",
 
-export const config_template = {
-    path: {
-        type: "string",
-        description: "Path of the sound file to load"
+    config_template: {
+        path: {
+            type: "string",
+            description: "Path of the sound file to load"
+        }
+    },
+
+    handle_push: async ({config}) => {
+        if (!config || !config.path) {
+            throw new Error("No path provided in config.");
+        }
+
+        // check the path is valid and make it absolute
+        if (!path.isAbsolute(config.path)) {
+            config.path = path.resolve(config.path);
+        }
+
+        if (!fs.existsSync(config.path)) {
+            throw new Error(`File does not exist at path: ${config.path}`);
+        }
+
+        await voicemeeter.init();
+        voicemeeter.login();
+
+        voicemeeter.setRawParameterString("Recorder.load", config.path);
+        voicemeeter.setRawParameterFloat("Recorder.play", 1);
+
+        // ensure sound is played before logging out
+        setTimeout(() => {
+            voicemeeter.logout();
+        }, 100);
     }
-};
-
-interface Options {
-    config?: {
-        path?: string;
-    }
-}
-
-export const handle_push = async ({config}: Options): Promise<void> => {
-    if (!config || !config.path) {
-        throw new Error("No path provided in config.");
-    }
-
-    // check the path is valid and make it absolute
-    if (!path.isAbsolute(config.path)) {
-        config.path = path.resolve(config.path);
-    }
-
-    if (!fs.existsSync(config.path)) {
-        throw new Error(`File does not exist at path: ${config.path}`);
-    }
-
-    await voicemeeter.init();
-    voicemeeter.login();
-
-    voicemeeter.setRawParameterString("Recorder.load", config.path);
-    voicemeeter.setRawParameterFloat("Recorder.play", 1);
-
-    // ensure sound is played before logging out
-    setTimeout(() => {
-        voicemeeter.logout();
-    }, 100);
-}
+} as Plugin;
